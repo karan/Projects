6bce6ca2ae91b2eebad1d32ed970969ea5e423a2
Karan Goel <karan@goel.im>
2013-07-11 07:23:01 +0530

    String reverse done

---
+++ Text/reverse.py
@@ -0,0 +1,8 @@
+# -*- coding: cp1252 -*-
+"""
+Reverse a String – Enter a string and the program
+will reverse it and print it out.
+"""
+
+string = raw_input("Whatchu wanna say to me? ")
+print "You say %s, I say %s" % (string, string[::-1])
