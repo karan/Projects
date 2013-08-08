0b7f69246b42e3b2e44d65c5cefb62be36e707e5
Karan Goel <karan@goel.im>
2013-08-08 15:18:37 +0530

    More pythonic way of creating dict

--- Text/count_vowels.py
+++ Text/count_vowels.py
@@ -4,14 +4,15 @@ the number of vowels in the text. For added complexity
 have it report a sum of each vowel found.
 """
 
-string = raw_input('Enter a string: ').lower()
+if __name__ == '__main__':
+    string = raw_input('Enter a string: ').lower()
 
-vowels = ['a', 'e', 'i', 'o', 'u']
-counts = dict(zip(vowels, [0, 0, 0, 0, 0]))
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    counts = dict(zip(vowels, [0]*5))
 
-for vowel in counts:
-    for char in string:
-        if vowel == char:
-            counts[vowel] += 1
+    for vowel in counts:
+        for char in string:
+            if vowel == char:
+                counts[vowel] += 1
 
-print counts
+    print counts
