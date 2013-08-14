0754e74aebe035652c98b91033018b2782d35c88
Karan Goel <karan@goel.im>
2013-08-14 17:42:53 +0530

    Safer way of user input

--- Numbers/calc.py
+++ Numbers/calc.py
@@ -1,22 +1,24 @@
-# -*- coding: cp1252 -*-
 """
 Calculator - A simple calculator to do basic operators.
 """
 
 if __name__ == '__main__':
-    num1 = input("Number 1: ")
-    num2 = input("Number 2: ")
-    op = raw_input("Operation (+, -, /, *): ")
-
-    if op not in '+-/*':
-        print "Invalid operator"
+    try:
+        num1 = int(raw_input("Number 1: "))
+        num2 = int(raw_input("Number 2: "))
+    except:
+        print 'Invalid input'
     else:
-        if op == '+':
-            res = num1 + num2
-        elif op == '-':
-            res = num1 - num2
-        elif op == '/':
-            res = num1 / num2
-        elif op == '*':
-            res = num1 * num2
-        print "%d %s %d = %d" % (num1, op, num2, res)
+        op = raw_input("Operation (+, -, /, *): ")
+        if op not in '+-/*':
+            print "Invalid operator"
+        else:
+            if op == '+':
+                res = num1 + num2
+            elif op == '-':
+                res = num1 - num2
+            elif op == '/':
+                res = num1 / num2
+            elif op == '*':
+                res = num1 * num2
+            print "%d %s %d = %d" % (num1, op, num2, res)
