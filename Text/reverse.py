d1a0c320483d8b5c94e9457a4384db3d4d10267f
Karan Goel <karan@goel.im>
2013-11-23 20:06:18 -0800

    Real reverse algorithm #31

--- Text/reverse.py
+++ Text/reverse.py
@@ -5,4 +5,7 @@ will reverse it and print it out.
 """
 
 string = raw_input("Whatchu wanna say to me? ")
-print "You say %s, I say %s" % (string, string[::-1])
+copy = [c for c in string]
+for i in range(len(copy) / 2):
+    copy[i], copy[len(copy) - i - 1] = copy[len(copy) - i - 1], copy[i]
+print "You say %s, I say %s" % (string, ''.join(copy))
