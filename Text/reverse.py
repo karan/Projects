85549073d1690c9078bc3aa45678a41d1a638129
jamesintonation <retrobanana.jn@gmail.com>
2013-12-03 20:44:10 -0700

    made reverse.py more Pythonic

--- Text/reverse.py
+++ Text/reverse.py
@@ -1,11 +1,11 @@
-# -*- coding: cp1252 -*-
+# -*- coding: cp1252, jamesintonation -*-
 """
 Reverse a String - Enter a string and the program
 will reverse it and print it out.
 """
 
 string = raw_input("Whatchu wanna say to me? ")
-copy = [c for c in string]
-for i in range(len(copy) / 2):
-    copy[i], copy[len(copy) - i - 1] = copy[len(copy) - i - 1], copy[i]
-print "You say %s, I say %s" % (string, ''.join(copy))
+print "You say %s, I say %s" % (string, string[::-1])
+
+# Reversing a string is easier than transforming it into an array, reversing the array, and rejoining it.
+# Just iterate over the string in reverse.
