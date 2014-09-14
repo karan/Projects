9ec9af49e1b8b95f2f1883e106f5c41cb225e863
JIGD <jigdlf1989@gmail.com>
2014-09-14 11:44:04 -0700

    Changed to use java's PI constant.

--- Solutions/src/findPiToTheNthDigit/Main.java
+++ Solutions/src/findPiToTheNthDigit/Main.java
@@ -6,17 +6,19 @@ public class Main {
 
 
 	public static void main(String[] args) {
-		String Pi = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652";
+		String Pi = Double.toString(Math.PI);
 		Scanner keyboard = new Scanner(System.in);
         int piDigits = 0;
+        boolean tooManyDigits;
         do {
-            System.out.println("Enter the desired number of digits for PI");
+            System.out.println("Enter the desired number of digits for PI from 0 to 15");
             piDigits = keyboard.nextInt();
-            if (piDigits > Pi.length()-3){
+            tooManyDigits = piDigits > Pi.length() - 2;
+            if (tooManyDigits){
                 System.out.println("unable to compute that many digits");
             }
         }
-        while(piDigits > Pi.length()-3);
+        while(tooManyDigits);
 		Pi = Pi.substring(0, piDigits+2);
 		System.out.println("PI = "+Pi);
 	}
