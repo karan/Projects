31399ff97f7b474660c8611b5afa80f328fc89bc
Nick Vasek <1amnick@Vulpine.local>
2014-11-23 16:25:43 -0500

    Initial Setup

---
+++ Projects in Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projects in Swift
+//
+//  Created by Nick Vasek on 11/23/14.
+//  Copyright (c) 2014 Nick Vasek. All rights reserved.
+//
+
+import Foundation
+
+println("testing")
+
