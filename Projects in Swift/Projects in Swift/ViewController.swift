00b7520e2a938b5245368c1b5c2436dd61928ab9
Nick Vasek <1amnick@Vulpine.local>
2014-11-23 18:31:05 -0500

    Changed template

---
+++ Projects in Swift/Projects in Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Projects in Swift
+//
+//  Created by Nick Vasek on 11/23/14.
+//  Copyright (c) 2014 Nick Vasek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Projects in Swift/Projects in SwiftTests/Projects_in_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Projects_in_SwiftTests.swift
+//  Projects in SwiftTests
+//
+//  Created by Nick Vasek on 11/23/14.
+//  Copyright (c) 2014 Nick Vasek. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Projects_in_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
