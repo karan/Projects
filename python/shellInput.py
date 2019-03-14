5a38e7e32d20ce4e4add4e9f1213b362e65a0045
Kai Hoth <hoth.kai@yahoo.com>
2019-03-14 22:01:18 +0100

    PIDecimals

--- python/shellInput.py
+++ python/shellInput.py
@@ -1,15 +1,20 @@
+import math
+
 def shell():
-    print("This piece of software shows Pi to a user definded decimal.")
+    print("This piece of software shows Pi to a user definded decimal. (max 15)")
 
     while True:
         print('>>> ', end='')
         entry = input()
-        if entry == "quit":
+        if entry == "quit" or entry == "q":
             break
         if not entry.isdigit():
             print("Thats not a Number. Please try again")
         else:
-            print(entry)
+            if int(entry) <= 15:
+                print(str(math.pi)[0:int(entry)+2])#
+            else:
+                print(math.pi, "| 15 is the max of after decimals")
 
 if __name__=='__main__':
     shell()
