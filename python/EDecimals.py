12037a18a44b4b38971811b80ff86089561a0e0f
Kai Hoth <hoth.kai@yahoo.com>
2019-03-14 22:05:21 +0100

    EDecimals

--- python/EDecimals.py
+++ python/EDecimals.py
@@ -1,7 +1,7 @@
 import math
 
 def shell():
-    print("This piece of software shows Pi to a user definded decimal. (max 15)")
+    print("This piece of software shows e to a user definded decimal. (max 15)")
 
     while True:
         print('>>> ', end='')
@@ -12,9 +12,9 @@ def shell():
             print("Thats not a Number. Please try again")
         else:
             if int(entry) <= 15:
-                print(str(math.pi)[0:int(entry)+2])
+                print(str(math.e)[0:int(entry)+2])
             else:
-                print(math.pi, "| 15 is the max of after decimals")
+                print(math.e, "| 15 is the max of after decimals")
 
 if __name__=='__main__':
     shell()
