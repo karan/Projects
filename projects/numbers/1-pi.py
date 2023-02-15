3e997576a722cb2c834599964a9cd2eb1e1d2cb9
MathieuFouillet <mathieu.fouillet94@gmail.com>
2023-02-15 16:44:02 +0100

    Rename file

--- projects/numbers/1-pi.py
+++ projects/numbers/1-pi.py
@@ -2,27 +2,25 @@
 import math
 import decimal
 
-def __getNthDecimalOfPi__(n: int) -> float:
+def __getNthDecimalOfPiFromMathModule__(n: int) -> float:
     a = pow(10, n)
     return math.trunc(math.pi * a) / a
 
 def test(f: callable, n: int)  :
-    return __getNthDecimalOfPi__(n) == f(n)
+    return __getNthDecimalOfPiFromMathModule__(n) == f(n)
 
 
-def getNthDecimalOfPiFromMathModule(n) :
+def getNthDecimalOfPi(n) :
     """
     Find PI to the Nth Digit using BPP formula
     https://fr.wikipedia.org/wiki/Formule_BBP#Et_pour_le_calcul_des_d%C3%A9cimales_?
     """
-    k = 0
     result = 0
-    while k < n :
+    for k in range(n) :
         result += 1 / (16**k) * (4/(8*k + 1) - 2/(8*k+4) -1/(8*k +5) - 1/(8*k+6))
-        k += 1
     return result
 
 
 for a in range(1000) : 
-    assert test(getNthDecimalOfPiFromMathModule, 1) != True
+    assert test(getNthDecimalOfPi, 1) != True
     print(f"Test {a} passed")
